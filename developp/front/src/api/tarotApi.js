fd00ea68af53065e95c3c56eabc967e9ed5d6483
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-07-31 17:05:12 +0200

    Add API call tarotApi.js

---
+++ developp/front/src/api/tarotApi.js
@@ -0,0 +1,28 @@
+// src/api/tarotApi.js : CRUD pour les cartes - Appels d'API avec axios
+
+import api from "./axios";
+
+// CRUD cards
+
+// Récupérer toutes les cartes
+export const fetchCards = async () => {
+  const response = await api.get("/cards");
+  return response.data;
+};
+
+// Créer une nouvelle carte
+export const createCard = async (cardData) => {
+  const response = await api.post("/cards", cardData);
+  return response.data;
+};
+
+// Mettre à jour une carte existante
+export const updateCard = async (id, cardData) => {
+  const response = await api.put(`/cards/${id}`, cardData);
+  return response.data;
+};
+
+// Supprimer une carte
+export const deleteCard = async (id) => {
+  await api.delete(`/cards/${id}`);
+};
