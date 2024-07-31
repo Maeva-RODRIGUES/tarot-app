4ff8844d42acaa9ebac7bdab6d525c166a746c35
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-07-31 16:52:10 +0200

    Install react query + react query devtools + add to main.jxs

--- developp/front/src/main.jsx
+++ developp/front/src/main.jsx
@@ -1,4 +1,4 @@
-// main.jsx
+// src/main.jsx
 
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -7,15 +7,23 @@ import App from "./App";
 import "./styles/index.css";
 import ChakraProviderWrapper from "./ChakraProviderWrapper";
 import { AuthProvider } from "./components/context/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Création d'un client React Query
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProviderWrapper>
       <Router>
         <AuthProvider>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </AuthProvider>
       </Router>
     </ChakraProviderWrapper>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
